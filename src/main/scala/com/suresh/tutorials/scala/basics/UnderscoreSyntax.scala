6a95c9196a996ac5cdff8f75e8ed71710fb8ae77
suresh mandalapu <iitkgp.suresh@gmail.com>
2019-07-21 21:51:40 +0100

    Added Unerscore syntax lecture

---
+++ src/main/scala/com/suresh/tutorials/scala/basics/UnderscoreSyntax.scala
@@ -0,0 +1,18 @@
+package com.suresh.tutorials.scala.basics
+
+object UnderscoreSyntax extends App {
+
+  var age: Int = _
+  println("age is " + age)
+
+  age = 100
+
+  println("age is " + age)
+
+  var aFlag: Boolean = _
+  println("aFlag is " + aFlag)
+
+  val newAge: Int = 10
+
+
+}
